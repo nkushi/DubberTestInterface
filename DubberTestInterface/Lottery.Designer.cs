e80b31bf8991155b48b049877d6b0352dbc2bb5e
ukush <ukushi01@gmail.com>
2022-05-31 12:23:40 +0100

    Add project files.

---
+++ DubberTestInterface/Lottery.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace DubberTestInterface
+{
+    partial class Lottery
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lottoNum1Label = new System.Windows.Forms.Label();
+            this.lottoNum2Label = new System.Windows.Forms.Label();
+            this.lottoNum3Label = new System.Windows.Forms.Label();
+            this.lottoNum4Label = new System.Windows.Forms.Label();
+            this.lottoNum5Label = new System.Windows.Forms.Label();
+            this.lottoNum6Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.BackColor = System.Drawing.Color.Lime;
+            this.btnGenerate.Font = new System.Drawing.Font("Showcard Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.Location = new System.Drawing.Point(235, 328);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(438, 64);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = false;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lottoNum1Label
+            // 
+            this.lottoNum1Label.AutoSize = true;
+            this.lottoNum1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lottoNum1Label.ForeColor = System.Drawing.Color.Black;
+            this.lottoNum1Label.Location = new System.Drawing.Point(28, 137);
+            this.lottoNum1Label.Name = "lottoNum1Label";
+            this.lottoNum1Label.Size = new System.Drawing.Size(93, 32);
+            this.lottoNum1Label.TabIndex = 1;
+            this.lottoNum1Label.Text = "label1";
+            // 
+            // lottoNum2Label
+            // 
+            this.lottoNum2Label.AutoSize = true;
+            this.lottoNum2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lottoNum2Label.Location = new System.Drawing.Point(166, 137);
+            this.lottoNum2Label.Name = "lottoNum2Label";
+            this.lottoNum2Label.Size = new System.Drawing.Size(93, 32);
+            this.lottoNum2Label.TabIndex = 2;
+            this.lottoNum2Label.Text = "label2";
+            // 
+            // lottoNum3Label
+            // 
+            this.lottoNum3Label.AutoSize = true;
+            this.lottoNum3Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lottoNum3Label.Location = new System.Drawing.Point(292, 137);
+            this.lottoNum3Label.Name = "lottoNum3Label";
+            this.lottoNum3Label.Size = new System.Drawing.Size(93, 32);
+            this.lottoNum3Label.TabIndex = 3;
+            this.lottoNum3Label.Text = "label3";
+            // 
+            // lottoNum4Label
+            // 
+            this.lottoNum4Label.AutoSize = true;
+            this.lottoNum4Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lottoNum4Label.Location = new System.Drawing.Point(425, 137);
+            this.lottoNum4Label.Name = "lottoNum4Label";
+            this.lottoNum4Label.Size = new System.Drawing.Size(93, 32);
+            this.lottoNum4Label.TabIndex = 4;
+            this.lottoNum4Label.Text = "label4";
+            // 
+            // lottoNum5Label
+            // 
+            this.lottoNum5Label.AutoSize = true;
+            this.lottoNum5Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lottoNum5Label.Location = new System.Drawing.Point(568, 137);
+            this.lottoNum5Label.Name = "lottoNum5Label";
+            this.lottoNum5Label.Size = new System.Drawing.Size(93, 32);
+            this.lottoNum5Label.TabIndex = 5;
+            this.lottoNum5Label.Text = "label5";
+            // 
+            // lottoNum6Label
+            // 
+            this.lottoNum6Label.AutoSize = true;
+            this.lottoNum6Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lottoNum6Label.Location = new System.Drawing.Point(705, 137);
+            this.lottoNum6Label.Name = "lottoNum6Label";
+            this.lottoNum6Label.Size = new System.Drawing.Size(93, 32);
+            this.lottoNum6Label.TabIndex = 6;
+            this.lottoNum6Label.Text = "label6";
+            // 
+            // Lottery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 450);
+            this.Controls.Add(this.lottoNum6Label);
+            this.Controls.Add(this.lottoNum5Label);
+            this.Controls.Add(this.lottoNum4Label);
+            this.Controls.Add(this.lottoNum3Label);
+            this.Controls.Add(this.lottoNum2Label);
+            this.Controls.Add(this.lottoNum1Label);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Lottery";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lottoNum1Label;
+        private System.Windows.Forms.Label lottoNum2Label;
+        private System.Windows.Forms.Label lottoNum3Label;
+        private System.Windows.Forms.Label lottoNum4Label;
+        private System.Windows.Forms.Label lottoNum5Label;
+        private System.Windows.Forms.Label lottoNum6Label;
+    }
+}
+
---
+++ DubberTestInterface/Lottery.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DubberTestInterface
+{
+    public partial class Lottery : Form
+    {
+        Label[] labelArray;
+        public Lottery()
+        {
+            InitializeComponent();
+            labelArray = new Label[] { lottoNum1Label, lottoNum2Label, lottoNum3Label, lottoNum4Label, lottoNum5Label, lottoNum6Label };
+            noLabels();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            
+            Random rndNo = new Random();
+            var intList = new List<int>();
+            
+            for (int i = 0; intList.Count < 6; i++)
+            {
+                int value = rndNo.Next(1, 49);
+
+                if (!intList.Contains(value))
+                {
+                    intList.Add(value);
+                }
+            }
+
+            intList.Sort();
+
+            for (int i = 0; i < 6; i++)
+            {
+                int number = intList[i];
+
+                labelArray[i].Text = number.ToString();
+
+                if (number < 10)
+                {
+                    labelArray[i].ForeColor = Color.Gray;
+                }
+                else if (9 < number && number < 20)
+                {
+                    labelArray[i].ForeColor = Color.Blue;
+                }
+                else if (19 < number && number < 30)
+                {
+                    labelArray[i].ForeColor = Color.Pink;
+                }
+                else if (29 < number && number < 40)
+                {
+                    labelArray[i].ForeColor = Color.Green;
+                }
+                else if (number > 39)
+                {
+                    labelArray[i].ForeColor = Color.Yellow;
+                }
+
+            }
+        }
+
+        //removes the default labels from showing on start-up
+         private void noLabels()
+         {
+
+            foreach(var labelText in labelArray)
+            {
+                labelText.Text = String.Empty;
+            }
+         }
+    }
+}
